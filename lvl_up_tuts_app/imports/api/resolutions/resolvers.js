d0b6897f0cf3b9780fced6540288bf9457cb5799
Jake Robbins <jrobbinsd@gmail.com>
2018-03-05 07:42:49 -0700

    added lodash and refactored resolvers

---
+++ lvl_up_tuts_app/imports/api/resolutions/resolvers.js
@@ -0,0 +1,17 @@
+
+export default{
+	Query: {
+		resolutions(){
+			return[
+				{
+				_id: "asdfasdf",
+				name: 'Get stuff Done!'
+			},
+				{
+					_id: "ffffff",
+					name: "Do some Squats!"
+				}
+			]
+		}
+	}
+};
