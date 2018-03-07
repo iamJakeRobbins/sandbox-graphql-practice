9c3a4760dade8b67e3d58df0c9e6c1183da2978d
Jake Robbins <jrobbinsd@gmail.com>
2018-03-07 14:53:01 -0700

    added goals

--- lvl_up_tuts_app/imports/startup/server/register-api.js
+++ lvl_up_tuts_app/imports/startup/server/register-api.js
@@ -1,28 +1,24 @@
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
-import merge from 'lodash/merge';
 
-import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
-import ResolutionsResolvers from '../../api/resolutions/resolvers'
-import UsersSchema from '../../api/users/User.graphql'
-import UsersResolvers from '../../api/users/resolvers'
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+import merge from "lodash/merge";
 
-//
-// hellos
-// test resume at vid 18
+import GoalsSchema from "../../api/goals/Goal.graphql";
+import GoalsResolvers from "../../api/goals/resolvers";
+import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
+import ResolutionsResolvers from "../../api/resolutions/resolvers";
+import UsersSchema from "../../api/users/User.graphql";
+import UsersResolvers from "../../api/users/resolvers";
 
-const typeDefs = [
-	ResolutionsSchema,
-	UsersSchema
-];
+// hkssslll
 
-const resolvers = merge(
-	ResolutionsResolvers, UsersResolvers
-)
+const typeDefs = [GoalsSchema, ResolutionsSchema, UsersSchema];
+
+const resolvers = merge(GoalsResolvers, ResolutionsResolvers, UsersResolvers);
 
 const schema = makeExecutableSchema({
-	typeDefs,
-	resolvers
+  typeDefs,
+  resolvers
 });
 
-createApolloServer({schema});
+createApolloServer({ schema });
