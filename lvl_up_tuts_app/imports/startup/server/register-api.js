166754aa5991661420d15fca5da20ea525bdfe77
Jake Robbins <jrobbinsd@gmail.com>
2018-03-07 13:46:17 -0700

    updated to accomodate user schema

--- lvl_up_tuts_app/imports/startup/server/register-api.js
+++ lvl_up_tuts_app/imports/startup/server/register-api.js
@@ -4,17 +4,23 @@ import merge from 'lodash/merge';
 
 import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
 import ResolutionsResolvers from '../../api/resolutions/resolvers'
+import UsersSchema from '../../api/users/User.graphql'
+import UsersResolvers from '../../api/users/resolvers'
+
+// 
 // hello
 // test resume at vid 18
 const testSchema = `
 type Query {
 	hi: String
 	resolutions: [Resolution]
+	user: User
 }
 `;
 const typeDefs = [
 	testSchema,
-	ResolutionsSchema
+	ResolutionsSchema,
+	UsersSchema
 ];
 
 const testResolvers = {
@@ -26,7 +32,7 @@ const testResolvers = {
 };
 
 const resolvers = merge(
-	testResolvers, ResolutionsResolvers
+	testResolvers, ResolutionsResolvers, UsersResolvers
 )
 
 const schema = makeExecutableSchema({
