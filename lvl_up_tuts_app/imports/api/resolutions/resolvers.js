4889bafb1a11bd36e23b0d4d750237e9bd098d2a
Jake Robbins <jrobbinsd@gmail.com>
2018-03-07 15:07:00 -0700

    updated resolvers cuz they were bad

--- lvl_up_tuts_app/imports/api/resolutions/resolvers.js
+++ lvl_up_tuts_app/imports/api/resolutions/resolvers.js
@@ -5,7 +5,6 @@ import Resolutions from './resolutions';
 export default{
 	Query: {
 		resolutions(obj, args, {userId}){
-			console.log(userId)
 			return Resolutions.find({userId}).fetch()
 		}
 	},
