d34b6c0144c46304d84040afcc441b44704a46bc
Jake Robbins <jrobbinsd@gmail.com>
2018-03-08 11:50:54 -0700

    not very exciting--fix this please gql!

--- lvl_up_tuts_app/imports/startup/server/register-api.js
+++ lvl_up_tuts_app/imports/startup/server/register-api.js
@@ -10,7 +10,7 @@ import ResolutionsResolvers from "../../api/resolutions/resolvers";
 import UsersSchema from "../../api/users/User.graphql";
 import UsersResolvers from "../../api/users/resolvers";
 
-// hkssslllstuff
+// hkssslllstuffand
 
 const typeDefs = [GoalsSchema, ResolutionsSchema, UsersSchema];
 
